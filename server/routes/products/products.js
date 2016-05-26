687683e7b2f017904bf0a359a2acc456ba9abf21
S.B. Lee <echo3042@gmail.com>
2016-05-26 16:49:08 -0700

    Implement product router

--- server/routes/products/products.js
+++ server/routes/products/products.js
@@ -8,15 +8,44 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  //
+  Product.find({}).then(function (docs) {
+    res.send(docs);
+  })
 });
 
 router.post('/', function(req, res){
-  //
+  var prod = req.body;
+  var newProduct = new Product({
+    type: prod.type,
+    title: prod.title,
+    description: prod.description,
+    price: prod.price,
+    locationInfo: prod.locationInfo,
+    author: prod.author,
+    isActivated: true
+  });
+  
+  newProduct.save().then(function (doc) {
+    console.log('doc saved', doc);
+    res.send({
+      data: doc,
+      message: 'New Product Created'
+    }).catch(function (err) {
+      console.log(err);
+    })
+  })
 });
 
 router.put('/:id', function(req, res){
-  //
+  var id = req.params.id;
+  var prod = req.body;
+  Product.findByIdAndUpdate(id, prod).then(function (doc) {
+    console.log(doc);
+    res.send(doc);
+  }).catch(function (err) {
+    console.log(err);
+    res.status(404).send('weird....');
+  });
 });
 
 
