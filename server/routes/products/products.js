967fde3e9090008706f0d58735938d08fbe1d4a7
S.B. Lee <echo3042@gmail.com>
2016-05-26 17:13:28 -0700

    Add Comment to products router

--- server/routes/products/products.js
+++ server/routes/products/products.js
@@ -1,18 +1,28 @@
-// import DB
-// import Product model
-// import User model
 var db = require("../../db/db.js");
 var User = require("../../db/user/user.js");
 var Product = require("../../db/product/product.js");
 var express = require('express');
 var router = express.Router();
 
+/**
+ *  Request Handler for GET(read) Method
+ *  @expected data with Req - nothing
+ *  @return {Array} - Array of every product Object
+ */
 router.get('/', function(req, res){
   Product.find({}).then(function (docs) {
     res.send(docs);
+  }).catch(function (err) {
+    console.log(err);
+    res.status(404).send('weird....');
   })
 });
 
+/**
+ *  Request Handler for POST(create) Method
+ *  @expected data with Req - Complete product data(type, title, description, price, locationInfo, author)
+ *  @return {Object} - contains every data including timestamps, ObjectId, isActivated
+ */
 router.post('/', function(req, res){
   var prod = req.body;
   var newProduct = new Product({
@@ -26,33 +36,27 @@ router.post('/', function(req, res){
   });
   
   newProduct.save().then(function (doc) {
-    console.log('doc saved', doc);
-    res.send({
-      data: doc,
-      message: 'New Product Created'
-    }).catch(function (err) {
+    res.send(doc).catch(function (err) {
       console.log(err);
-    })
+      res.status(404).send('weird....');
+    });
   })
 });
 
+/**
+ *  Request Handler for PUT(update) Method
+ *  @expected data with Req - 1. ObjectId as parameter(req.params.id)
+ *                            2. Complete product data including the field need to be updated(req.body)
+ */
 router.put('/:id', function(req, res){
   var id = req.params.id;
   var prod = req.body;
   Product.findByIdAndUpdate(id, prod).then(function (doc) {
-    console.log(doc);
-    res.send(doc);
+    res.end();
   }).catch(function (err) {
     console.log(err);
     res.status(404).send('weird....');
   });
 });
 
-
-// create helper method
-  // get all listing
-  // delete(deactivate) listing - should be synced with User model
-  // post new listing - should be synced with User model
-  
-// export router
 module.exports = router;
