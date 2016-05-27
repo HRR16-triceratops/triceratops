59cdc9f05b52975b3c6c04de20c0af10eeee877d
Pallandor <roger.sejas@gmail.com>
2016-05-27 10:54:30 +0930

    Removed temp server

--- server/server.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-1
-app.use(express.static(__dirname + '/../build'));
-
-app.listen(8000, function(){
-	console.log('server listening on port 8000!'); 
-}); 
