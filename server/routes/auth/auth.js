2587a2c65aade035783b0dd8af5e70f5cc2f1ca9
Brent Colson <bacolson@gmail.com>
2016-05-26 21:10:27 -0400

    Signup in Auth router now working, without JWT

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -5,7 +5,14 @@ var User = require("../../db/user/user.js");
 var express = require('express');
 var router = express.Router();
 
+// var generateToken(user)
+
 router.get('/login', function(req, res){
+  User.find({}, function(err,body){
+    res.send(body);
+  });
+});
+
   // User.find({email: req.body.email})
   // })
   // .then(comparePassword(savedPassword, req.body.password))
@@ -14,26 +21,39 @@ router.get('/login', function(req, res){
   // })
   // .catch(function(){
   //   res.redirect('/login');
-});
+
 
 router.get('/logout', function(req, res){
-  //
+
 });
 
 
 router.post('/signup', function(req, res){
-  // var password = hashPassword(req.body.password);
-  // var newUser = new User({
-  //   email: req.body.email,
-  //   password: password
-  // });
-  // newUser.save()
-  //   .then(function(){
-  //   res.end();
-  //   })
-  //   .catch(function(err){
-  //     console.log(err);
-  //   });
+
+  var user = req.body;
+  console.log(user.displayName)
+  User.findOne({username: user.username})
+    .exec(function(err,found){
+    if(!found){
+      var newUser = new User({
+        username: user.username,
+        password: user.password,
+        displayName: user.displayName,
+        email: user.email
+      });
+      newUser.save(function(err, newUser){
+        if(err){
+          res.status(500).send(err);
+        } else {
+          console.log("user created!");
+          res.end();
+        }
+      });   
+    } else {
+      console.log("Account already exists");
+      res.redirect('/login');
+    }
+  });
 });
 
 
