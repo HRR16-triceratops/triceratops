e838a00d04c714a6dc87b4acc98b076c592f35b3
Brent Colson <brentcolson@gmail.com>
2016-05-27 22:25:02 -0400

    Completed Get and Put helper functions and added documentation comments

--- client/services/helper.js
+++ client/services/helper.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
 
+/**
+ *  Access JWT token from user's browser. If user has no token,
+ *  set equal to 'default' which the server will register as invalid
+ *  and redirect to login.
+ */
 var localStorageToken = local.storage.getItem('token') ? local.storage.getItem('token') : 'default';
 
+
+/**
+ *  Post helper function for making api calls to server
+ *  @expected arguments - Url, data (i.e. object of user data or product data)
+ *  @return {Object}
+ */
 var postHelper = function(url, data){
-  //check to see if this token data is accessible this way and what it returns
+  
   axios.post(url, {
     headers: {
       Authorization: "Bearer " + localStorageToken
@@ -15,33 +26,38 @@ var postHelper = function(url, data){
   });
 };
 
-var getHelper = function(url, user){
-    axios.post(url, {
-    headers: {
-      Authorization: "Bearer " + localStorageToken
-    }
-  })
-  .then(function(res){
-    return res;
-  });
+/**
+ *  Get helper function for making api calls to server
+ *  @expected arguments - Url
+ *  @return {Object}
+ */
+var getHelper = function(url){
+    axios.get(url, {
+      headers: {
+        Authorization: "Bearer " + localStorageToken
+      }
+    })
+    .then(function(res){
+      return res;
+    });
 };
 
-
-var putHelper = function(url, id, user){
+/**
+ *  Put helper function for making api calls to server to update data in db
+ *  @expected arguments - Url, data (i.e. object of user data or product data)
+ *  @return {Object}
+ */
+var putHelper = function(url, data){
   axios.put(url, {
-    params: {
-      ID: id
-    },
-    data: user
-  });
-  axios.post(url, {
     headers: {
       Authorization: "Bearer " + localStorageToken
     },
-    body: data
+    params: {
+      ID: data.id
+    },
+    data: data
   })
   .then(function(res){
     return res;
-  }); 
-
+  });
 };
