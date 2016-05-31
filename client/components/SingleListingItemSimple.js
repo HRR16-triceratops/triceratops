c0d41c94a3c8dd43c53d775f63e3ca3309b0ed69
Pallandor <roger.sejas@gmail.com>
2016-06-01 03:12:39 +0930

    Adding structure to components

---
+++ client/components/SingleListingItemSimple.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import { Link } from 'react-router'; 
+
+// Refactor out to even smaller components! 
+// Use props to populate relevant component info e.g. img src url, Link pointer, 
+// blurb with title, etc. 
+
+const SingleListingItemSimple = (props) => {
+	return (
+		<div>
+			<h3>Item: {props.item.title}</h3>
+			<img src="http://bit.ly/1UrCXd2"/>
+			<p><b>Description: </b>{props.item.description}</p>
+			<button><Link to={"/listings/" + props.item.id}>Show Details</Link></button>
+		</div>
+		)
+};
+
+export default SingleListingItemSimple;
