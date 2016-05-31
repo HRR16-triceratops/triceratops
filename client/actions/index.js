563d1bab9e17acb4549ebf4e4fe1a9e4bcd86445
S.B. Lee <echo3042@gmail.com>
2016-05-31 13:48:29 -0700

    Add new Action & Action creators

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,11 +1,43 @@
 import * as types from '../constants/ActionTypes';
+import helper from '../services/helper';
 
 // action creators
-const makeLoginRequest = () => {
+const makeLoginRequest = (userData) => {
+  var url = '/auth/login';
+  var request = helper.postHelper(url, userData);
+
 	return {
-		type: types.LOGIN_REQUEST
+		type: types.LOGIN_REQUEST,
+    payload: request
 	};
-}
+};
+
+const loginSuccess = (user) => {
+  return {
+    type: types.LOGIN_SUCCESS,
+    payload: user
+  };
+};
+
+const loginFailure = (err) => {
+  return {
+    type: types.LOGIN_FAILURE,
+    payload: err
+  };
+};
+
+const makeSignupRequest = (userData) => {
+  return {
+    type: types.SIGNUP_REQUEST,
+    payload: userData
+  };
+};
+
+const logOut = () => {
+  return {
+    type: types.LOGOUT
+  };
+};
 
 // thunks
 attemptLogin(username,password){
--- client/containers/Login.js
+++ client/containers/Login.js
@@ -1,17 +1,39 @@
+import React from 'react';
 import { Component } from 'react';
-import React from 'react'; 
 
 class Login extends Component {
+  constructor(props){
+    super(props);
+  }
 
-	constructor(props){
-		super(props);
-	}
+  render(){
+    return (
+      <div>
+         <div class="well bs-component">
+          <form class="form-horizontal">
+            <fieldset>
+              <legend>Signin</legend>
+              <div class="form-group">
+                <label for="inputEmail" class="col-md-2 control-label">Email</label>
 
-	render(){
-		return (
-				<div>Login component here</div>
-			)
-	}
+                <div class="col-md-10">
+                  <input type="email" class="form-control" id="inputEmail" placeholder="Email" />
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputPassword" class="col-md-2 control-label">Password</label>
+
+                <div class="col-md-10">
+                  <input type="password" class="form-control" id="inputPassword" placeholder="Password" />
+                </div>
+              </div>
+              <button class="btn btn-success-outline" type="submit">Login</button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login; 
+export default Login;
