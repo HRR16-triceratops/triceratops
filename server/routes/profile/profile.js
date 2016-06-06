9f37c9821db7bb6d161dd48834ec366e41c077ee
Brent Colson <bacolson@gmail.com>
2016-06-06 13:46:20 -0400

    deleted client side auth in profile router and added comments to profile update API put method

--- server/routes/profile/profile.js
+++ server/routes/profile/profile.js
@@ -5,27 +5,6 @@ var router = express.Router();
 var secret = process.env.JWT_SECRET || 'sleepingpuppies';
 
 /**
- *  Request Handler for GET(read) Method
- *  @expected data with Req - nothing
- *  @expected Header with Req - { "Authorization": "Bearer <JWT_TOKEN>"}
- *  @return {Array} - Array of every product Object
- */
-
- // Does this API needed????
-router.get('/:username', expressJwt({secret: secret}), function(req, res){
-  var username = req.params.username;
-  User.findOne({username: username}).then(function (user) {
-    delete user._id;
-    delete user.password;
-    delete user.__v;
-    res.send(user);
-  }).catch(function (err) {
-    console.log(err);
-    res.status(404).send('weird....');
-  });
-});
-
-/**
  *  Request Handler for PUT(update) Method
  *  @expected data with Req - 1. ObjectId as parameter(req.params.id)
  *                            2. Complete user data including the field need to be updated(req.body)
@@ -42,6 +21,13 @@ router.put('/:id', expressJwt({secret: secret}), function(req, res){
   });
 });
 
+
+/**
+ *  Request Handler for Put(update) Method
+ *  @expected data with Req - updated user information
+ *  @expected Header with Req - { "Authorization": "Bearer <JWT_TOKEN>"}
+ *  @expected Params in URL - User ID
+ */
 router.put('/rent/:id', expressJwt({secret: secret}), function(req, res){
   var id = req.params.id;
   var user = req.body;
