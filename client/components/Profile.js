6d46c6532c577eadb5355a3ddf60817f32d9d971
Brent Colson <bacolson@gmail.com>
2016-06-07 00:14:54 -0400

    Completed refactor of profile component

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -7,6 +7,7 @@ import { List, ListItem } from 'material-ui/List';
 import FlatButton from 'material-ui/FlatButton';
 import ProfileCard from './ProfileCard.js';
 
+
 class ProfileComponent extends Component {
 
   constructor(props){
@@ -28,65 +29,24 @@ class ProfileComponent extends Component {
     return (
       <div>
         <ProfileCard 
-          expanded={true} 
-          title={"Profile"} 
-          subtitle={"Edit email, password..."}
-          displayName={user.username}
-          email={user.email}
-          linkTo={'/'}
-          buttonLabel={"Edit Profile"}
+          cardType={'profile'}
+          user={user}
         />
         <ProfileCard 
+          cardType={'listings'}
+          user={user}
+          products={products}
           expanded={false} 
-          title={"Your Listings"} 
-          subtitle={"View and manage your listings"}
-          displayName={user.username}
-          email={user.email}
           linkTo={'/manage'}
           buttonLabel={"Manage your listings"}
-          listItems={
-            products.items.filter(item => {
-              return item.author === user.username;
-            }).map(item => {
-              return (
-                 <Link to={"/listings/" + item._id}>
-                        <ListItem
-                          key={item._id}
-                          primaryText={item.title}
-                          secondaryText={"$"+item.price + " - " + item.summary}
-                        />
-                 </Link>
-              )
-            })
-          }
+
         />
-        <Card className="profileCards">
-          <CardHeader
-            title="Upcoming Rentals"
-            subtitle="Check out the details of your upcoming rentals"
-            actAsExpander={true}
-            showExpandableButton={true}
-          />
-          <CardText expandable={true}>
-            <List style={{paddingTop: 0}}>
-              {
-                this.props.products.items.filter(item => {
-                  return item.rentSchedule.length === 0 ? false : item.rentSchedule[0].username === this.props.user.username;
-                }).map(item => {
-                  return (
-                     <Link to={"/listings/" + item._id}>
-                            <ListItem
-                              key={item._id}
-                              primaryText={item.title}
-                              secondaryText={"$"+item.price + " - " + item.summary}
-                            />
-                     </Link>
-                  )
-                })
-              }
-            </List>
-          </CardText>
-        </Card>
+        <ProfileCard 
+          cardType="rentals"
+          expanded={false} 
+          user={user}
+          products={products}
+        />
       </div>
       );
   }
--- client/components/ProfileCard.js
+++ client/components/ProfileCard.js
@@ -1,33 +1,123 @@
 import React from 'react';
+import { Component } from 'react';
 import { Link } from 'react-router';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import { List, ListItem } from 'material-ui/List';
 import FlatButton from 'material-ui/FlatButton';
 
-const ProfileCard = (props) => {
+export default class ProfileCard extends Component{
   
-  return (
-    <Card className="profileCards" initiallyExpanded={props.expanded}>
-      <CardHeader
-        title={props.title}
-        subtitle={props.subtitle}
-        actAsExpander={true}
-        showExpandableButton={true}
-      />
-      <CardText expandable={true} style={{paddingTop: 0, paddingBottom: 0}}>
-        <List style={{paddingTop: 0}}>
-          {
-            props.listItems
-          }
-        </List>
-      </CardText>
-      <CardActions expandable={true} style={{paddingTop: 0}}>
-        <Link style={{paddingTop: 0}} to={props.linkTo}>
-          <FlatButton style={{paddingTop: 0}} label={props.buttonLabel} />
-        </Link>
-      </CardActions>
-    </Card>
-  );
-};
+  constructor(props){
+    super(props);
+  }
 
-export default ProfileCard;
+  renderProfile(){
+    if(this.props.cardType === "profile"){
+      return (
+        <Card className="profileCards" initiallyExpanded={true}>
+          <CardHeader
+            title={"Profile"}
+            subtitle={"Edit email, password ..."}
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true} style={{paddingTop: 0, paddingBottom: 0}}>
+            {this.props.cardType === "profile"}
+            <p><strong>Username:</strong> {this.props.user.username}</p>
+            <p><strong>Email:</strong> {this.props.user.email}</p>
+          </CardText>
+          <CardActions expandable={true} style={{paddingTop: 0}}>
+          </CardActions>
+        </Card>
+      )
+    }
+  }
+
+  renderListings(){
+    if(this.props.cardType === 'listings'){
+      return (
+        <Card className="profileCards" initiallyExpanded={this.props.expanded}>
+          <CardHeader
+            title={"Your Listings"}
+            subtitle={"View and manage your listings"}
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true} style={{paddingTop: 0, paddingBottom: 0}}>
+            {
+              this.props.products.items.filter(item => {
+                return item.author === this.props.user.username;
+              }).map(item => {
+                return (
+                   <Link to={"/listings/" + item._id}>
+                          <ListItem
+                            key={item._id}
+                            primaryText={item.title}
+                            secondaryText={"$"+item.price + " - " + item.summary}
+                          />
+                   </Link>
+                )
+              })
+            }
+          </CardText>
+          <CardActions expandable={true} style={{paddingTop: 0}}>
+            <Link to={"/Manage/"}>
+              <FlatButton label={"Manage your listings"} />
+            </Link>
+          </CardActions>
+        </Card>
+      )
+    }
+  }
+
+
+  renderRentals(){
+    if(this.props.cardType === "rentals"){
+      return (
+        <Card className="profileCards" initiallyExpanded={this.props.expanded}>
+          <CardHeader
+            title={"Upcoming Rentals"}
+            subtitle={"Check out the details of your upcoming rentals"}
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText expandable={true} style={{paddingTop: 0, paddingBottom: 0}}>
+            {
+              this.props.products.items.filter(item => {
+                return item.rentSchedule.length === 0 ? false : item.rentSchedule[0].username === this.props.user.username;
+              }).map(item => {
+                return (
+                   <Link to={"/listings/" + item._id}>
+                          <ListItem
+                            key={item._id}
+                            primaryText={item.title}
+                            secondaryText={"$"+item.price + " - " + item.summary}
+                          />
+                   </Link>
+                )
+              })
+            }
+          </CardText>
+          <CardActions expandable={true} style={{paddingTop: 0}}>
+          </CardActions>
+        </Card>
+      )
+    }
+  }
+  render(){
+    return(
+      <div>
+        <div>
+          {this.renderProfile()}
+        </div>
+        <div>
+          {this.renderListings()}
+        </div>
+        <div>
+          {this.renderRentals()}
+        </div>
+      </div>
+    )
+
+  }
+};
